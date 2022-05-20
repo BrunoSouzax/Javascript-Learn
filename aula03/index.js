fa5cd2a0c46af5d1f4ad6fa14068b7b535508259
BrunoSouzax <bruno_sdsouza@hotmail.com>
2022-05-20 17:23:02 -0300

    conteudo

--- aula03/index.js
+++ aula03/index.js
@@ -1,4 +1,4 @@
-const btn = document.getElementById('btn');
+/* const btn = document.getElementById('btn');
 
 btn.addEventListener('click', function onClick(event) {
   // 👇️ change background color
@@ -6,5 +6,39 @@ btn.addEventListener('click', function onClick(event) {
 
   // 👇️ optionally change text color
   // document.body.style.color = 'white';
+}); */
+
+/* const btnA = document.getElementById('btn');
+
+btnA.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('box');
+
+  box.style.backgroundColor = 'coral';
+
+  // 👇️ optionally change text color
+  // box.style.color = 'white';
+}); */
+
+/* const btnA = document.getElementById('btn');
+
+btnA.addEventListener('click', function onClick(event) {
+  console.log(event.target);
+
+  // 👇️ change background color
+  event.target.style.backgroundColor = 'salmon';
+
+  // 👇️ optionally change text color
+  // event.target.style.color = 'white';
+}); */
+
+const btnA = document.getElementById('btn');
+
+btnA.addEventListener('click', function onClick(event) {
+  // 👇️ change background color
+  event.target.style.backgroundColor = 'salmon';
+
+  // 👇️ optionally change text color
+  // event.target.style.color = 'white';
 });
+
 
