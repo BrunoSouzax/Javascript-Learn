7915cd35ea9856d0fca546cfef6b80b0ec6aa6cf
BrunoSouzax <bruno_sdsouza@hotmail.com>
2022-05-22 11:38:20 -0300

    aula05

---
+++ aula06-bgMenu/script.js
@@ -0,0 +1,8 @@
+const blue = window.document.getElementById('blue')
+const black = window.document.getElementById('black')
+const white = window.document.getElementById('white')
+
+blue.addEventListener('click', function onclick(event){
+    document.body.style.backgroundColor = 'blue'
+    document.body.style.backgroundColor = 'lightskyblue'
+})
